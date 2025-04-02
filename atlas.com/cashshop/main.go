444cb358f7fd45e4fb42f62bf354a724197378f4
Chronicle20 <a.chronicle.20@gmail.com>
2025-04-02 07:24:37 -0400

    Initial commit.

---
+++ atlas.com/cashshop/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"atlas-cashshop/logger"
+	"atlas-cashshop/service"
+	"atlas-cashshop/tracing"
+	"github.com/Chronicle20/atlas-rest/server"
+)
+
+const serviceName = "atlas-cashshop"
+
+type Server struct {
+	baseUrl string
+	prefix  string
+}
+
+func (s Server) GetBaseURL() string {
+	return s.baseUrl
+}
+
+func (s Server) GetPrefix() string {
+	return s.prefix
+}
+
+func GetServer() Server {
+	return Server{
+		baseUrl: "",
+		prefix:  "/api/cashshop/",
+	}
+}
+
+func main() {
+  l := logger.CreateLogger(serviceName)
+	l.Infoln("Starting main service.")
+	
+	tdm := service.GetTeardownManager()
+
+	tc, err := tracing.InitTracer(l)(serviceName)
+	if err != nil {
+		l.WithError(err).Fatal("Unable to initialize tracer.")
+	}
+
+	server.CreateService(l, tdm.Context(), tdm.WaitGroup(), GetServer().GetPrefix())
+	
+	tdm.TeardownFunc(tracing.Teardown(l)(tc))
+
+	tdm.Wait()
+	l.Infoln("Service shutdown.")
+}
